9d225d9ebbd538566f094d8000f6a3326385827e
flschaefer <flschaefer@users.noreply.github.com>
2015-08-17 10:29:00 +0300

    Refactored the code to make it run...

--- pitch_extraction/spectral_pitch_extractor.py
+++ pitch_extraction/spectral_pitch_extractor.py
@@ -1,6 +1,7 @@
 ﻿from brian import *
 from brian.hears import *
 
+
 class SpectralPitchExtractor(object):
     """
     A computational model of the human's hearing pitch extraction stage, using the spacial model
@@ -12,56 +13,52 @@ class SpectralPitchExtractor(object):
         """
         pass
 
-    def extract(self,a,cf=[],Nf=3000,fs=44100):
-        #Channel frequinces are unnessesary as well as Nf
-        if cf.size==0:
-           cf = erbspace(20*Hz, 20*kHz, Nf)
+    def extract(self, spikes, sample_rate):
+        Nf = 3000
+        # Channel frequinces are unnessesary as well as Nf
+        cf = erbspace(20 * Hz, 20 * kHz, Nf)
         # TODO: Compute excitatory pattern (for each auditory nerve, sum up along the time dimension)
-        sa=np.sum(a,axis=0)
-        log_sa=np.log2(sa)
-        
+        sa = np.sum(spikes, axis=0)
+        log_sa = np.log2(sa)
+
         # TODO: Use a pitch estimation method like pattern Matching, wightman, goldstein, Terhardt,...
 
-        #I took Terhardt's virtual pitch
-        peaks_lst=peaks(log_sa,Nf)      
-        N=peaks_lst.size    
-    
-        w=1.    
-        index_score=np.zeros(N)
-        H=0
-    
-        for i in range (N):
-            for j in range (N):
-                if peaks_lst[j]!=0:                
-                    H+=w/(j+1.)*(peaks_lst[i]+np.log2(j+1))            
-            index_score[i]=H
-            H=0             
+        # I took Terhardt's virtual pitch
+        peaks_lst = self.peaks(log_sa, cf, Nf)
+        N = peaks_lst.size
+
+        w = 1.
+        index_score = np.zeros(N)
+        H = 0
+
+        for i in range(N):
+            for j in range(N):
+                if peaks_lst[j] != 0:
+                    H += w / (j + 1.) * (peaks_lst[i] + np.log2(j + 1))
+            index_score[i] = H
+            H = 0
 
         # TODO: Return pitch estimate
         return cf[index_score.tolist().index(max(index_score))]
-    
 
     # Function for extraction peaks in spikes activity
-    def peaks(spikes_sum,Nf=3000):
-        e = spikes_sum/np.max(spikes_sum)        
-        l = int(100*Nf/3000.)   
-        w =np.exp(-((np.arange(l)-l/2)/float(l))**2 *20.)
-        
-        e = np.convolve(e, w, mode='same')    
-        peaks=np.zeros(e.shape)
-        grad=np.diff(e)
-        
+    def peaks(self, spikes_sum, cf, Nf=3000):
+        e = spikes_sum / np.max(spikes_sum)
+        l = int(100 * Nf / 3000.)
+        w = np.exp(-((np.arange(l) - l / 2) / float(l)) ** 2 * 20.)
+
+        e = np.convolve(e, w, mode='same')
+        peaks = np.zeros(e.shape)
+        grad = np.diff(e)
+
         v = np.zeros((len(e),))
-        v[:len(e)-2] = ((np.sign(grad[0:-1])>0)*(np.sign(grad[1:])<0))*(np.abs(grad[0:-1]*grad[1:]))
-        v = v/np.max(v)
-        r=np.zeros(v.shape)
-        j=0
+        v[:len(e) - 2] = ((np.sign(grad[0:-1]) > 0) * (np.sign(grad[1:]) < 0)) * (np.abs(grad[0:-1] * grad[1:]))
+        v = v / np.max(v)
+        r = np.zeros(v.shape)
+        j = 0
         for i in v:
-            if i>0:
-                r[j]=cf[i]
-            j+=1    
-          
+            if i > 0:
+                r[j] = cf[i]
+            j += 1
+
         return r
-
-
-
