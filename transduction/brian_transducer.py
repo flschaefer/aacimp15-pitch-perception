3338110da8e42218613ea8383dd87999d5679115
vincentadam87 <vincent.adam87@gmail.com>
2015-08-17 16:43:01 +0300

    update transducer

--- transduction/brian_transducer.py
+++ transduction/brian_transducer.py
@@ -5,6 +5,7 @@ from transducer import Transducer
 
 import numpy as np
 from matplotlib import pyplot as plt
+from scipy.signal import kaiserord, lfilter, firwin, freqz
 
 
 class BrianTransducer(Transducer):
@@ -17,7 +18,7 @@ class BrianTransducer(Transducer):
 
     def get_spikes(self,samples, sample_rate):
 
-        cf = erbspace(20*Hz, 20*kHz, self.n_channels)
+        cf = erbspace(100*Hz, 8*kHz, self.n_channels)
 
         sound = Sound(samples, samplerate=sample_rate*Hz)
 
@@ -40,13 +41,34 @@ class BrianTransducer(Transducer):
 
     def get_cochleogram(self,samples, sample_rate):
 
-        cf = erbspace(20*Hz, 20*kHz, self.n_channels)
+        cf = erbspace(100*Hz, 8*kHz, self.n_channels)
+        # Hair cells transduction parameters (low pass filter)
+        width_hz = 3000.0
+        # The desired attenuation in the stop band, in dB.
+        ripple_db = 60.0
+        # The cutoff frequency of the filter.
+        cutoff_hz = 1000.0
+
+        taps = kaiser_design(sample_rate, width_hz, ripple_db, cutoff_hz)
+
         sound = Sound(samples, samplerate=sample_rate*Hz)
         fb = Gammatone(sound, cf)
-        output = fb.process()
         f = lambda x: 3 * clip(x, 0, Inf) ** (1.0/3.0)
-        a = f(output)
-        return a
+        lphwr =  filtered_x = lfilter(taps, 1.0, f(fb.process())  ,axis=0)
+        #lphwr =  f(fb.process())
+
+        return lphwr
+
+
+def kaiser_design(fs,width_hz,ripple_db,cutoff_hz):
+    # The Nyquist rate of the signal.
+    nyq_rate = fs / 2.0
+    width = width_hz/nyq_rate
+    # Compute the order and Kaiser parameter for the FIR filter.
+    N, beta = kaiserord(ripple_db, width)
+    # Use firwin with a Kaiser window to create a lowpass FIR filter.
+    taps = firwin(N, cutoff_hz/nyq_rate, window=('kaiser', beta))
+    return taps
 
 if __name__ == '__main__':
 
